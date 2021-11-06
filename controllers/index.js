ac80c03d9878e38edad8961f2ec391f57d6b4498
radopeti <rado.peti@gmail.com>
2021-11-06 13:12:46 +0100

    Add redirect

--- controllers/index.js
+++ controllers/index.js
@@ -2,3 +2,8 @@
 exports.index = function(req, res, next) {
     res.render('index', { title: 'KExpress' });
 };
+
+exports.submit_lead = function(req, res, next) {
+    console.log("reqest body:", req.body.lead_email);
+    res.redirect('/');
+};
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 let index = require('../controllers/index');
 /* GET home page. */
 router.get('/', index.index);
+router.post('/', index.submit_lead)
 
 module.exports = router;
