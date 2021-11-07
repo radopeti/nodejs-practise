901e3389d2968c7ca629ddfdc05d52e5ce0b93db
radopeti <rado.peti@gmail.com>
2021-11-07 13:14:28 +0100

    implement delete lead

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,5 @@ router.get('/leads', index.show_leads);
 router.get('/leads/:lead_id', index.show_lead)
 router.get('/leads/:lead_id/edit', index.show_edit_lead)
 router.post('/leads/:lead_id/edit', index.edit_lead)
-
+router.post('/leads/:lead_id/delete', index.delete_lead)
 module.exports = router;
