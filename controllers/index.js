0adeae7527af850ff44a18aee8a3afb2582638f4
radopeti <rado.peti@gmail.com>
2021-11-07 11:24:05 +0100

    save email to db

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,15 @@
 
+const models = require('../models')
+
 exports.index = function(req, res, next) {
     res.render('index', { title: 'KExpress' });
 };
 
 exports.submit_lead = function(req, res, next) {
-    console.log("reqest body:", req.body.lead_email);
-    res.redirect('/');
+    console.log("reqest body:", );
+    models.Lead.create({
+        email: req.body.lead_email
+    }).then(result => {
+        res.redirect('/');
+    })
 };
